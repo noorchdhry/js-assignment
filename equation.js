94f4fb82a6c4debb1b5632993ccf0bdb198b5e7d
Noor <noor.m.chaudhry@gmail.com>
2019-04-11 16:14:09 -0400

    Initial
    First commit for the various files needed for the assignment

---
+++ equation.js
@@ -0,0 +1,15 @@
+
+const num1 = Object.querySelector("#num1")
+const num2 = Object.querySelector("#num2")
+let operator = Object.querySelector("#operator")
+
+let operator = {
+    add,
+    subtract,
+    multiple,
+    divide
+}
+
+operator.add = (num1, num2) => num1 + num2
+
+
