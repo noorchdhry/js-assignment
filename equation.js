becf9d2ab6a6b4a53cb43e80e7da2cb7ebf819fd
Noor <noor.m.chaudhry@gmail.com>
2019-04-11 19:05:33 -0400

    initializeAdd function
    created initializeAdd function to get random numbers that print to the screen using prototyping

--- equation.js
+++ equation.js
@@ -1,15 +1,34 @@
 
-const num1 = Object.querySelector("#num1")
-const num2 = Object.querySelector("#num2")
-let operator = Object.querySelector("#operator")
+const question = document.querySelector('#question');
+const message = document.querySelector('#message');
+let inputValue = document.querySelector('#inputValue');
+let correctAnswer;
 
-let operator = {
-    add,
-    subtract,
-    multiple,
-    divide
+function operations() {}
+ 
+
+operations.prototype.initializeAddition = function(){
+    num1 = Math.floor(Math.random() * 10); 
+    num2 = Math.floor(Math.random() * 10); 
+    console.log("lkj")
+    question.textContent =  num1 + " + " + num2 + " ?";
+    correctAnswer = num1 + num2;
 }
 
-operator.add = (num1, num2) => num1 + num2
+const o = new operations;
+const addBtn = document.querySelector('#add');
+addBtn.onclick = o.initializeAddition;
+
+function checkAnswer(){
+    let guess = Number(inputValue.value);
+
+    if(correctAnswer === guess){
+        
+        message.textContent =  "You got it!!";
+    }
+    else {
+        message.textContent =  "Sorry that was't quite right, try again.";
+    }
+}
 
 
