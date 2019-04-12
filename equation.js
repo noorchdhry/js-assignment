684bbacb7fce5e7f20471680184e3681b725b33e
Noor <noor.m.chaudhry@gmail.com>
2019-04-12 15:28:51 -0400

    Other operations
    Added other operationor funtions such as add, subtract, multiple and divide

--- equation.js
+++ equation.js
@@ -2,32 +2,83 @@
 const question = document.querySelector('#question');
 const message = document.querySelector('#message');
 let inputValue = document.querySelector('#inputValue');
+let score = document.querySelector('#score');
 let correctAnswer;
+let scoreNum = 0;
+
+function createRandomNums(){
+    num1 = Math.floor(Math.random() * 10); 
+    num2 = (Math.floor(Math.random() * 10)) + 1; 
+}
 
 function operations() {}
- 
 
+ 
+//Create the random generating Add
 operations.prototype.initializeAddition = function(){
-    num1 = Math.floor(Math.random() * 10); 
-    num2 = Math.floor(Math.random() * 10); 
-    console.log("lkj")
-    question.textContent =  num1 + " + " + num2 + " ?";
+    createRandomNums();
+    question.textContent =  num1 + " + " + num2 + " = ?";
     correctAnswer = num1 + num2;
+}
+
+operations.prototype.initializeSubtraction = function(){
+    createRandomNums();
+    question.textContent =  num1 + " - " + num2 + " = ?";
+    correctAnswer = num1 - num2;
+}
+
+operations.prototype.initializeDivision = function(){
+    do{
+       num1 = Math.floor(Math.random() * 10); 
+        num2 = (Math.floor(Math.random() * 10)) + 1; 
+        r = num1 % num2;
+    } while(r === 0)
+     createRandomNums();
+       question.textContent =  num1 + " / " + num2 + " = ?";
+        correctAnswer = num1 / num2; 
+        
+}
+
+operations.prototype.initializeMultiplication = function(){
+    createRandomNums();
+    question.textContent =  num1 + " X " + num2 + " = ?";
+    correctAnswer = num1 * num2;
 }
 
 const o = new operations;
 const addBtn = document.querySelector('#add');
 addBtn.onclick = o.initializeAddition;
 
+const subtractBtn = document.querySelector('#subtract');
+subtractBtn.onclick = o.initializeSubtraction;
+
+const divideBtn = document.querySelector('#divide');
+divideBtn.onclick = o.initializeDivision;
+
+const multiplyBtn = document.querySelector('#multiply');
+multiplyBtn.onclick = o.initializeMultiplication;
+
+function restartNum(){
+    num1 = "";
+    num2 = "";
+    correctAnswer = null;
+    inputValue.value = "";
+    question.textContent = "# ? #";
+}
+
 function checkAnswer(){
     let guess = Number(inputValue.value);
 
     if(correctAnswer === guess){
-        
-        message.textContent =  "You got it!!";
+        scoreNum++;
+        message.textContent =  ("You got it!! Your score is now" + scoreNum);
+        score.textContent = scoreNum;
+        restartNum()
     }
     else {
-        message.textContent =  "Sorry that was't quite right, try again.";
+        scoreNum--;
+        message.textContent =  ("Sorry that was't quite right, try again." + scoreNum);
+        score.textContent = scoreNum;
     }
 }
 
